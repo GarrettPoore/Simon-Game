cf309e39c4aa9e8f8f1f2e01bbde7119c39ac98f
GarrettPoore <garrettjpoore@gmail.com>
2017-08-16 21:29:44 -0400

    Added initial elements

--- scripts.js
+++ scripts.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  
+  $(".color_button").click(function(event){
+    var color = event.target.id;
+    console.log(color + " pressed");
+  });
+
+  $("#restart").click(function(){
+    console.log("Restart clicked");
+  });
+
+  $("#strict").click(function(){
+    console.log("Strict mode toggled");
+  });
 });
---
+++ simon.js
@@ -0,0 +1,14 @@
+//TODO - random series of button presses
+//Each time user enters all correctly, add another to the series
+
+//Hear sound (based on color) on correct press
+//Notified of incorrect press & restart thee sequence again
+
+//Display current number of steps
+
+//Restart button
+
+//Strict mode:
+//-On fail, notify player and restart at 1 with new sequence
+
+//Win at 20 presses
