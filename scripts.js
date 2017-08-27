f32f6f5458365da7a65932176414d698925a2bba
GarrettPoore <garrettjpoore@gmail.com>
2017-08-27 19:21:10 -0400

    Added more styles and dynmaic resizing

--- scripts.js
+++ scripts.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+  resizeGame();
+
   $(".color_button").click(function(event){
     if (!Simon.pause) {
       var color = event.target.id;
@@ -26,4 +28,22 @@ $(document).ready(function() {
     console.log(this.checked);
     Simon.strict = this.checked;
   });
+
+  $(window).resize(function(){
+    resizeGame();
+  });
 });
+
+function resizeGame() {
+  var width = window.innerWidth;
+  console.log(width);
+  if (width < 625) {
+    var newWidth = Math.max(450, width - 25)
+    $("body").get(0).style.setProperty("--game_size", newWidth + "px");
+  } else {
+    var size = window.getComputedStyle(document.body).getPropertyValue("--game_size");
+    if (size != 600) {
+      $("body").get(0).style.setProperty("--game_size", "600px");
+    }
+  }
+}
